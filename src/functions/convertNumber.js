2f109f080e64c4da5d728c5ad5835aa15ab9cce4
venkatsammm <venkatkishoreraman@gmail.com>
2024-05-22 14:49:55 +0530

    crypto

--- src/functions/convertNumber.js
+++ src/functions/convertNumber.js
@@ -2,6 +2,7 @@ export const convertNumber = (number) => {
     const numberWithCommas = number.toLocaleString();
     var arr = numberWithCommas.split(",");
     if (arr.length == 5) {
+      
       //Trillions
       return arr[0] + "." + arr[1].slice(0, 2) + "T";
     } else if (arr.length == 4) {
