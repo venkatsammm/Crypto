93cc43d96f182df62199cb9cc6e5639f77ae91b8
venkatsammm <venkatkishoreraman@gmail.com>
2024-06-07 19:14:43 +0530

    finished

--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,46 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 import Button from "../components/Common/Button";
 import Header from "../components/Common/Header";
 import TabsComponent from "../components/Dashboard/Tabs";
-import PasswordSetup from "../Password/Password";
-import PasswordInput from "../Password/PasswordInput";
 import { get100Coins } from "../functions/get100Coins";
 
 function Watchlist() {
+  const watchlist = JSON.parse(localStorage.getItem("watchlist"));
   const [coins, setCoins] = useState([]);
-  const [watchlist, setWatchlist] = useState([]);
-  const [passwordSet, setPasswordSet] = useState(false);
-  const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
-    const storedPasswordSet = sessionStorage.getItem("passwordSet") === "true";
-    const storedAuthenticated = sessionStorage.getItem("authenticated") === "true";
-
-    setPasswordSet(storedPasswordSet);
-    setAuthenticated(storedAuthenticated);
-  }, []);
-
-  useEffect(() => {
-    const fetchWatchlist = async () => {
-      if (authenticated) {
-        // Fetch watchlist from local storage
-        const storedWatchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
-        setWatchlist(storedWatchlist);
-      }
-    };
-
-    fetchWatchlist();
-  }, [authenticated]);
-
-  useEffect(() => {
-    if (watchlist.length > 0) {
+    if (watchlist) {
       getData();
-    } else {
-      setCoins([]); // Clear the coins if watchlist is empty
     }
-  }, [watchlist]);
+  }, []);
 
   const getData = async () => {
     const allCoins = await get100Coins();
@@ -49,53 +21,28 @@ function Watchlist() {
     }
   };
 
-  const handleRemoveFromWatchlist = (coinId) => {
-    const updatedWatchlist = watchlist.filter(id => id !== coinId);
-    setWatchlist(updatedWatchlist);
-    // Update watchlist in local storage
-    localStorage.setItem("watchlist", JSON.stringify(updatedWatchlist));
-    toast.info("Item removed from the watchlist");
-  };
-
-  const handlePasswordSet = () => {
-    setPasswordSet(true);
-    sessionStorage.setItem("passwordSet", "true");
-  };
-
-  const handlePasswordCorrect = () => {
-    setAuthenticated(true);
-    sessionStorage.setItem("authenticated", "true");
-  };
-
   return (
     <div>
       <Header />
-      <ToastContainer />
-      {!passwordSet ? (
-        <PasswordSetup onPasswordSet={handlePasswordSet} />
-      ) : !authenticated ? (
-        <PasswordInput onPasswordCorrect={handlePasswordCorrect} />
+      {watchlist?.length > 0 ? (
+        <TabsComponent coins={coins} />
       ) : (
-        watchlist.length > 0 ? (
-          <TabsComponent coins={coins} onRemoveFromWatchlist={handleRemoveFromWatchlist} />
-        ) : (
-          <div>
-            <h1 style={{ textAlign: "center" }}>
-              Sorry, No Items In The Watchlist.
-            </h1>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                margin: "2rem",
-              }}
-            >
-              <a href="/dashboard">
-                <Button text="Dashboard" />
-              </a>
-            </div>
+        <div>
+          <h1 style={{ textAlign: "center" }}>
+            Sorry, No Items In The Watchlist.
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "2rem",
+            }}
+          >
+            <a href="/dashboard">
+              <Button text="Dashboard" />
+            </a>
           </div>
-        )
+        </div>
       )}
     </div>
   );
